3cd41d1a4719299e26ff777707c2c63067354a42
dionebraz <dionebraz@outlook.com>
2022-06-22 10:58:33 -0300

    Adicionada animaçôes ao selecionar os cards

--- resources/js/game.js
+++ resources/js/game.js
@@ -56,13 +56,21 @@ baralho.sort(() => {
 const body = document.querySelector("body");
 const grade = document.querySelector("#grade");
 const spots = document.querySelector("#spots");
-const playerName = (document.querySelector("#playerName").innerHTML =
-  localStorage.getItem("player"));
+const lastSpots = document.querySelector('#last-spots')
+const playerName = (document.querySelector("#playerName").innerHTML = localStorage.getItem("player"));
+
 let score;
 let escolhidos = [];
 
 function criarGrade() {
+
+  
   score = 0;
+  spots.innerHTML = score
+
+  if(lastSpots > score) {
+    lastSpots.innerHTML;
+  }
 
   for (let i = 0; i < baralho.length; i++) {
     let card = document.createElement("img");
@@ -79,6 +87,8 @@ function escolherCard() {
   card.src = baralho[card.id].img;
   escolhidos.push(card);
 
+  card.classList.toggle('front')
+
   if (escolhidos.length == 2) {
     setTimeout(() => {
       let card1 = escolhidos[0];
@@ -86,15 +96,17 @@ function escolherCard() {
 
       if (card1.name == card2.name && card1.id != card2.id) {
         selectionSound()
-        
+
         card1.removeEventListener("click", escolherCard);
         card2.removeEventListener("click", escolherCard);
         card1.style.filter = "grayscale(100%)";
         card2.style.filter = "grayscale(100%)";
         score++;
+        spots.innerHTML = score
       } else {
         card1.src = "../images/back.png";
         card2.src = "../images/back.png";
+        card.classList.toggle('front')
       }
 
       if (score == baralho.length / 2) {
@@ -107,6 +119,8 @@ function escolherCard() {
           body.appendChild(resetButton);
           resetButton.addEventListener("click", () => {
             document.location.reload(true);
+            lastSpots = score
+            if(lastSpots == score) {}
           });
         }, 500);
       }
